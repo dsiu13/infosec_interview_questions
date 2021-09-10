241e8e1cba3b27dfd4b84e248a613f73fd1ca1e5
IamZombiez <dereksiu216@gmail.com>
2021-09-10 14:13:01 -0700

    need to add uppercase char logic

--- caesar_cipher.js
+++ caesar_cipher.js
@@ -4,26 +4,74 @@
 // A = 65
 // Z = 90
 
+
 //Shift backwards first?
 //Decrypt function
 
-let caesarCipher = (message, shift_val) => {
+let caesarCipher = (message, shiftVal) => {
 
-    lettersArr = message.split("")
+    let cipherArr = []
 
-    for(i = lettersArr.length - 1; i >= 0; i--){
-      console.log(message.charCodeAt(i) + shift_val)
+    for(i = message.length - 1; i >= 0; i--){
+
+      if (message.charCodeAt(i) + shiftVal > 122){
+         let decNum = message.charCodeAt(i) + shiftVal - 122 + 96
+         let numChar = String.fromCharCode(decNum)
+         cipherArr.push(numChar)
+      } else {
+         let decNum = message.charCodeAt(i) + shiftVal
+         let numChar = String.fromCharCode(decNum)
+         cipherArr.push(numChar)
+      }
+    }
+    console.log(cipherArr.join(""))
+};
+
+caesarCipher("zed", 1)
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+//Shift backwards first?
+//Decrypt function
+
+let rotCipher = (message) => {
+
+    for(i = message.length - 1; i >= 0; i--){
+      console.log(message.charCodeAt(i) + 13)
       // console.log(String.fromCharCode(message.charCodeAt(i)))
     }
 
 };
 
-caesarCipher("test string", 0)
+//set to start value
+if (message.charCodeAt(i) + 13 > 122){
+   (122 - (message.charCodeAt(i) + 13)) + 97
 
-
-if (message.charCodeAt(i) + shift_val > 122){
-   (122 - (message.charCodeAt(i) + shift_val)) + 97
-
-} else if(message.charCodeAt(i) + shift_val < 97){
+} else if(message.charCodeAt(i) + 13 < 97){
 
 }
+
+
+
+
+
+
+let caesarCipher = (message) => {
+
+    for(i = message.length - 1; i >= 0; i--){
+
+      if (message.charCodeAt(i) + 1 > 122){
+         console.log((message.charCodeAt(i) + 1 - 122) + 64)
+      } else {
+        console.log(message.charCodeAt(i) + 1)
+     // console.log(String.fromCharCode(message.charCodeAt(i)))
+      }
+
+    }
+
+};
+
+caesarCipher("zed")
